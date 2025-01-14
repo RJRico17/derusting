4e6b8817c5c5728518522446b0f15f81873be520
rjfredr <fredrickalexander1727@gmail.com>
2025-01-14 10:22:43 -0800

    commit 01.14.25 10:22am 1/7

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,22 +1,13 @@
 public class ArrayPractice {
   public static void main(String[] args) {
-    // Create an array of Strings of size 4
-
-    // Set the value of the array at each index to be a different String
-    // It's OK to do this one-by-one
-
-    // Get the value of the array at index 2
-
-    // Get the length of the array
-
-    // Iterate over the array using a traditional for loop and print out each item
-
-    // Iterate over the array using a for-each loop and print out each item
-
-    /*
-     * Reminder!
-     * 
-     * Arrays start at index 0
-     */
+    String[] cueists = {"Efren","Francisco","Johann","AJ"};
+    System.out.println(cueists[2]);
+    System.out.println(cueists.length);
+    for (int i=0; i < cueists.length; i++) {
+      System.out.println(cueists[i]);
+    }
+    for (String cueist : cueists) {
+      System.out.println(cueist);
+    }
   }
 }
