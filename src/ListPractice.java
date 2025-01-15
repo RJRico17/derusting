835ea83899bec02b61162091e03862172ceaeb56
rjfredr <fredrickalexander1727@gmail.com>
2025-01-14 18:20:15 -0800

    01.14.25 6:20pm 2/7

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,36 +1,31 @@
+import java.util.*;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
-    // Create an empty ArrayList of Strings and assign it to a variable of type List
+    
+    ArrayList<String> poolEquipment = new ArrayList<String>();
+    poolEquipment.add("Cue");
+    poolEquipment.add("Pool Balls");
+    poolEquipment.add("Chalk");
+    System.err.println(poolEquipment);
 
-    // Add 3 elements to the list (OK to do one-by-one)
+    System.out.println(poolEquipment.get(1));
+    poolEquipment.set(1,"Pool Table");
+    System.err.println(poolEquipment);
+    poolEquipment.add(0, "Glove");
+    System.err.println(poolEquipment);
 
-    // Print the element at index 1
+    for (int i=0;i<poolEquipment.size();i++) {
+      System.out.println(poolEquipment.get(i));
+    }
 
-    // Replace the element at index 1 with a new value
-    // (Do not insert a new value. The length of the list should not change)
+    poolEquipment.sort(null);
+    System.out.println(poolEquipment);
 
-    // Insert a new element at index 0 (the length of the list will change)
-
-    // Check whether the list contains a certain string
-
-    // Iterate over the list using a traditional for-loop.
-    // Print each index and value on a separate line
-
-    // Sort the list using the Collections library
-
-    // Iterate over the list using a for-each loop
-    // Print each value on a second line
-
-    /*
-     * Usage tip!
-     * 
-     * Use a traditional for-loop when you need to use the index or you need to iterate in an
-     * unconventional order (e.g. backwards)
-     * 
-     * Otherwise, if you're iterating the in the conventional order and don't need the
-     * index values a for-each loop is cleaner.
-     */
+    for (String equipment : poolEquipment) {
+        System.out.println(equipment);
+    }
   }
 }
